5fd0d640e8ebfaefdcf78761d2b473a53ad210db
Elena-Alexandra Secara <e.a.secara@edu.salford.ac.uk>
2021-03-14 19:44:01 +0000

    RESTAPI

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
-const app     = express();
+const app = express();
 
 const nutrition = []
+var myFCalls = 0;
 
 app.get('/', function(req, res) {
+    myFCalls++
     // the scraping magic will happen here
-    let recipeName = 'vegan-kale-pesto-pasta'
+    let recipeName = req.query.recipeName
     let carbsValue = req.query.carbsValue
 
     let url = 'https://www.bbcgoodfood.com/recipes/' + recipeName
     console.log(url)
+    console.log('The function has been called: ' + myFCalls)
 
     var $;
     request(url, function(error, response, html) {
-        if(!error){
+        if (!error) {
             $ = cheerio.load(html)
             var prediction = $('div.header__body > h1.header__title').text()
             carbsValue = $('tbody.key-value-blocks__batch > tr.key-value-blocks__item')
@@ -23,27 +26,30 @@ app.get('/', function(req, res) {
             carbsValue.each(function() {
                 const ntr = $(this).find('td.key-value-blocks__value').text();
                 nutrition.push(ntr)
-              })
-            
+            })
+
             var json = {
-                prediction: prediction,
-                calories: nutrition[0],
-                fat: nutrition[1],
-                saturates: nutrition[2],
-                carbs: nutrition[3],
-                sugars: nutrition[4],
-                fibre: nutrition[5],
-                protein: nutrition[6],
-                salt: nutrition[7]
+                id: 0,
+                // prediction: prediction,
+                nutritions: [
+                    { calories: nutrition[0] },
+                    { fat: nutrition[1] },
+                    { saturates: nutrition[2] },
+                    { carbs: nutrition[3] },
+                    { sugars: nutrition[4] },
+                    { fibre: nutrition[5] },
+                    { protein: nutrition[6] },
+                    { salt: nutrition[7] }
+                ]
             }
 
             res.send(json)
         }
-    })  
+    })
 })
 
-app.listen('8080')
-console.log('API is running on: http://localhost:8080')
+app.listen('8000')
+console.log('API is running on: http://localhost:8000')
 
 
 module.exports = app
