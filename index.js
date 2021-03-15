571e12400109b03d1dea94f531fa20cd42568a8e
Elena-Alexandra Secara <e.a.secara@edu.salford.ac.uk>
2021-03-15 21:22:42 +0000

    fix(index): update get endpoint to web scrape for the recipe categories

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cheerio = require('cheerio');
 const app = express();
 
 var nutrition = []
+var titlesArray = []
 var myFunc = 0
 
 app.get('/', function(req, res) {
@@ -14,9 +15,10 @@ app.get('/', function(req, res) {
     let title = req.query.title
     let recipeName = req.query.recipeName
     let carbsValue = req.query.carbsValue
+    let mainTitle = req.query.mainTitle
+    let link = req.query.link
 
-    let url = 'https://www.bbcgoodfood.com/recipes/' + recipeName.replace(/\s+/g, '-').toLowerCase()
-    console.log(url)
+    let url = 'https://www.bbcgoodfood.com/search/recipes?q=' + recipeName.replace(/\s+/g, '-').toLowerCase()
 
     var $;
     request(url, function(error, response, html) {
@@ -24,6 +26,21 @@ app.get('/', function(req, res) {
             $ = cheerio.load(html)
             title = $('div.post-header__body > h1.post-header__title').text()
             carbsValue = $('tbody.key-value-blocks__batch > tr.key-value-blocks__item')
+            link = $('a.standard-card-new__article-title').attr('href')
+            console.log("link: " + link)
+            mainTitle = $('h4.standard-card-new__display-title')
+            mainTitle.each(function() {
+                const titles = $(this).find('a.standard-card-new__article-title').text()
+                titlesArray.push(titles)
+                
+                //console.log(titles)
+                if(titles.includes(recipeName))
+                {
+                    titlesArray = []
+                    titlesArray.push(titles)
+                }
+                // check when to rewrite in the array
+            })
 
             carbsValue.each(function() {
                 const ntr = $(this).find('td.key-value-blocks__value').text();
@@ -35,6 +52,7 @@ app.get('/', function(req, res) {
             })
 
             var json = {
+                allTitles: titlesArray,
                 id: 0,
                 title: title,
                 nutritions: nutrition
@@ -46,7 +64,7 @@ app.get('/', function(req, res) {
 })
 
 app.listen('8000')
-console.log('API is running on: http://localhost:8000/?recipeName=flat-apple-vanilla-tart')
+console.log('API is running on: http://localhost:8000/?recipeName=cupcakes')
 
 
 module.exports = app
